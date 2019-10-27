07f435259fbf40137a580d1b08a2cd160b803620
Patrick Nyatindo <nyatindopatrick@gmail.com>
2019-10-27 18:04:21 +0300

    fix server error and add dotenv

--- src/server/server.js
+++ src/server/server.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const app = express();
+
+const db = process.env.DB_KEY;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose
-  .connect(
-    "mongodb+srv://Pato:dogobigy@97@cluster0-irfd0.gcp.mongodb.net/fikasafe?retryWrites=true&w=majority",
-    { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(db, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("DB connected sucessfully"))
   .catch(err => console.log(err));
 
 app.use(express.static("dist"));
-
-
 
 app.use("/api", require("./routes"));
 
