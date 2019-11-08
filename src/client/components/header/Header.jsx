0541e732bdf85df8895479c764661a58d1badde0
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-11-08 19:04:39 +0300

    add and style the navigation item

--- src/client/components/header/Header.jsx
+++ src/client/components/header/Header.jsx
@@ -8,11 +8,46 @@ const Header = () => {
   };
   return (
     <header className="header">
-      <div className="header__search">Search...</div>
-      <div className="header__avatar">Your face</div>
-      <button className="btn btn-primary" onClick={handleLogout}>
+      <div className="header__search ">
+        <i className="fas fa-search icon"></i>
+        <input type="text" placeholder="Search..." name="search" />
+      </div>
+      <div className="header_generic_items">
+        {/* icon */}
+        <span className="icon">
+          <i className="fas fa-comments"></i>
+        </span>
+        {/* notification bell */}
+        <div className="__dropdown">
+          <span className="icon">
+            <i className="fas fa-bell"></i>
+            <input type="text" placeholder="9" className="notify_icon" />
+          </span>
+          <div className="dropdown__content">
+            <p>Hello World!</p>
+          </div>
+        </div>
+
+        {/* username */}
+
+        <div className="__dropdown">
+          <span className="user_name">
+            John Doe
+            <img
+              src="../../../../public/assets/images/avator.jpg"
+              alt="Avatar"
+              className="avatar_face"
+            />
+          </span>
+          <div className="dropdown__content">
+            <p>Hello World!</p>
+          </div>
+        </div>
+      </div>
+
+      {/* <button className="btn btn-primary" onClick={handleLogout}>
         Logout
-      </button>
+      </button> */}
     </header>
   );
 };
